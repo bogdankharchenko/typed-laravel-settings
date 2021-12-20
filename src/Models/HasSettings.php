25f042e0f294a9ac0769f1f6f6a5dc727950eb89
bogdankharchenko <bogdankharchenko@users.noreply.github.com>
2021-12-20 16:55:14 +0000

    Fix styling

--- src/Models/HasSettings.php
+++ src/Models/HasSettings.php
@@ -11,14 +11,12 @@ use Illuminate\Support\Traits\ReflectsClosures;
 
 trait HasSettings
 {
-
     use ReflectsClosures;
 
     public function settings(): MorphMany
     {
         return $this->morphMany(Setting::class, 'settable');
     }
-
 
     public function setSettings(?Closure $closure = null): self
     {
@@ -36,21 +34,19 @@ trait HasSettings
         return $this;
     }
 
-
     public function getSettings($class)
     {
         return new $class($this);
     }
 
-
     public function scopeWhereSettings(Builder $builder, $class, $name, $operator, $value = null)
     {
         $class = ClassMorphMap::getKeyFromClass($class);
 
-        $builder->whereHas('settings', function($builder) use ($class, $name, $operator, $value) {
+        $builder->whereHas('settings', function ($builder) use ($class, $name, $operator, $value) {
             $builder->where('class', $class);
 
-            $builder->where(function(Builder $builder) use ($name, $operator, $value) {
+            $builder->where(function (Builder $builder) use ($name, $operator, $value) {
                 $builder->where("payload->{$name}", $operator ?? '=', $value ?? $operator);
             });
         });
--- src/Providers/SettingsServiceProvider.php
+++ src/Providers/SettingsServiceProvider.php
@@ -6,12 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class SettingsServiceProvider extends ServiceProvider
 {
-
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../../config/typed-settings.php', 'typed-settings');
     }
-
 
     public function boot()
     {
